4b5572ebe2396adc81eb1feef4798672204912c8
PerezContrerasLuis <luisperezcontreras@gmail.com>
2021-08-11 13:15:58 -0500

    Migracion y modelo para tabla personas

---
+++ app/Models/Persona.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nombre','tipo_documento','num_documento','direccion','telefono','email'];
+}
